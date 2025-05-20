a170965a0e96fa982331eca0e5cfa4c4e4fcf80c
kkmanqlo <jomech23@bemen3.cat>
2025-05-20 16:34:39 +0200

    Web funcional

--- RageQuit_Web/Assets/Scripts/UI/MainMenu/ExitButton.cs
+++ RageQuit_Web/Assets/Scripts/UI/MainMenu/ExitButton.cs
@@ -1,11 +1,13 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitButton : MonoBehaviour
 {
     // Este script se encarga de manejar el botón de salir del juego en el menú principal.
     public void Exit()
     {
-        Application.Quit();
-        Debug.Log("Saliendo del juego...");
+        SceneManager.LoadScene("MenuPrincipal");
+        DOTween.KillAll();
     }
 }
